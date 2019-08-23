2cd115527112264cc2f49a3adbc36ab2113de36a
netsecninja <netsecninja@users.noreply.github.com>
2019-08-23 15:32:39 -0600

    Add files via upload

---
+++ findurls/findurls.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+'''
+Title: Find URLs
+Version: 1.0
+Created by: Jeremiah Bess - jeremiah.bess@gmail.com
+Description: Script find and output all URLs in office documents
+Resources:
+* https://towardsdatascience.com/how-to-extract-data-from-ms-word-documents-using-python-ed3fbb48c122
+'''
+
+# Imports
+import zipfile # Manipulate zip files (Office docs)
+import re # Search for URLs
+import argparse # Parse command arguments
+
+# Global variables
+ignores = ['schemas','purl.org','w3.org']  # Whitelisted strings in URLs
+
+# Functions
+def getZip():  # Argument parser
+    global zipped
+    parser = argparse.ArgumentParser(description='Find URLs script')
+    parser.add_argument('filename', nargs=1, help='Provide a filename to search for URLs')
+    args = parser.parse_args()
+    filename = args.filename[0]
+    try:
+        zipped = zipfile.ZipFile(filename)
+        return zipped
+    except:
+        print('Unable to open document:', args.filename[0])
+        exit()
+
+def getLinks():  # Collect all http/s URLs
+    global zipped
+    linklist = []
+    for file in zipped.namelist():
+        content = str(zipped.read(file))
+        links = re.findall('http.*?[^"]+',content)
+        for link in links:
+            if not checkIgnores(link):  # Omit URLs with whitelisted strings
+                if link not in linklist:  # Verify link does not already exist
+                    linklist.append(link)                    
+                    print(link)
+    return
+
+def checkIgnores(link):  # Omit URLs with whitelisted strings
+    for ignore in ignores:
+        if ignore in link:
+            return True
+    return False
+
+# Main
+if __name__ == '__main__':
+    getZip()
+    getLinks()
+
